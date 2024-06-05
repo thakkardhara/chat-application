eb5b42a00c2d227bab9f97ba1f6f7d6e10d20b03
dharathakkarz <dharat@zignuts.com>
2024-06-05 17:47:35 +0530

    display

--- frontend/src/components/messages/MessageDisplay.jsx
+++ frontend/src/components/messages/MessageDisplay.jsx
@@ -24,10 +24,10 @@ const MessageDisplay = () => {
       ) :(
         <>
         {/* header */}
-        {/* <div className="bg-slate-500 px-4 py-2 mb-2">
+        <div className="bg-slate-500 px-4 py-2 mb-2">
           <span className="label-text">To:</span>
           <span className="text-gray-900 font-bold">{selectedConversation.fullname}</span>
-        </div> */}
+        </div>
 {/* 
 <div className="bg-slate-500 px-4 py-2 mb-2 flex justify-between items-center">
     <div>
