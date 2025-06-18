26c56683eb6a84a37915fa9ee3968aa43b9f1643
thakkardharaine <dhara.ineinfotech@gmail.com>
2025-06-18 17:55:46 +0530

    error solved

--- frontend/src/components/messages/MessageDisplay.jsx
+++ frontend/src/components/messages/MessageDisplay.jsx
@@ -1,37 +1,34 @@
-import { TiMessages } from "react-icons/ti";
-import MessageInput from "./MessageInput";
-import Messages from "./Messages";
-import useConversation from "../../zustand/UseConversation";
-import { useEffect } from "react";
-import { useAuthContext } from "../../context/AuthContext";
-import useMobileView from "../../zustand/UseMobileView";
+import {TiMessages} from 'react-icons/ti'
+import MessageInput from "./MessageInput"
+import Messages from "./Messages"
+import useConversation from '../../zustand/UseConversation'
+import { useEffect } from 'react'
+import { useAuthContext } from '../../context/AuthContext'
 // import { MdAddIcCall } from "react-icons/md";
 // import { FaVideo } from "react-icons/fa";
 const MessageDisplay = () => {
   // const noChats = false;
 
-  const { isMobileView, setMobileView } = useMobileView();
-  const { selectedConversation, setSelectedConversation } = useConversation();
+  const {selectedConversation,setSelectedConversation}= useConversation();
 
   //after logout pehla nu selected nai reh
-  useEffect(() => {
-    return () => setSelectedConversation(null);
-  }, [setSelectedConversation]);
+  useEffect(()=>{
+    return()=> setSelectedConversation(null)
+
+  },[setSelectedConversation])
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
       {!selectedConversation ? (
-        <NoChats />
-      ) : (
+        <NoChats/>
+      ) :(
         <>
-          {/* header */}
-          <div className="bg-slate-500 px-4 py-2 mb-2">
-            <span className="label-text"></span>
-            <span className="text-gray-900 font-bold">
-              {selectedConversation.fullname}
-            </span>
-          </div>
-          {/* 
+        {/* header */}
+        <div className="bg-slate-500 px-4 py-2 mb-2">
+          <span className="label-text"></span>
+          <span className="text-gray-900 font-bold">{selectedConversation.fullname}</span>
+        </div>
+{/* 
 <div className="bg-slate-500 px-4 py-2 mb-2 flex justify-between items-center">
     <div>
         <span className="label-text">To:</span>
@@ -42,26 +39,31 @@ const MessageDisplay = () => {
         <FaVideo className="text-white" />
     </div>
 </div> */}
-          <Messages />
-          <MessageInput />
-        </>
-      )}
+        <Messages />
+        <MessageInput />
+      </>
+      )}   
+  
     </div>
-  );
-};
+  )
+}
 
-export default MessageDisplay;
+export default MessageDisplay
+
 
 export const NoChats = () => {
-  const { authUser } = useAuthContext();
 
-  return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 {authUser.fullname} ❄</p>
-        <p>Select a chat to start messaging</p>
-        <TiMessages className="text-3xl md:text-6xl text-center" />
-      </div>
-    </div>
-  );
+  const {authUser} = useAuthContext();
+
+	return (
+		<div className='flex items-center justify-center w-full h-full'>
+			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome 👋 {authUser.fullname}  ❄</p>
+				<p>Select a chat to start messaging</p>
+				<TiMessages className='text-3xl md:text-6xl text-center' />
+			</div>
+		</div>
+	);
 };
+
+
