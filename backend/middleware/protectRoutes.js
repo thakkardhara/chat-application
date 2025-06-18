5cec4ab85a89b4b84f53326747a5c7bfbe69cd3b
thakkardharaine <dhara.ineinfotech@gmail.com>
2025-06-18 16:08:58 +0530

    log

--- backend/middleware/protectRoutes.js
+++ backend/middleware/protectRoutes.js
@@ -4,21 +4,26 @@ import { configDotenv } from 'dotenv';
 
 const protectRoutes = async(req,res,next)=>{
     try {
+        console.log("Cookies received:", req.cookies);
         const token = req.cookies.jwt;
         if(!token){
+            console.log("❌ No token found in cookies");
             return res.status(401).json({error:"unauthorized- no token provided"})
         }
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
         if(!decoded){
+             console.log("✅ Decoded token:", decoded);
             return res.status(401).json({error:"unauthorized- no token provided"})
         }
 
         const user = await User.findById(decoded.userId).select("-password")
         if(!user){
+             console.log("❌ User not found");
             return res.status(401).json({error:"User not found"})
             }
 
         req.user = user
+        onsole.log("✅ User authenticated:", user.username);
         next()
   
     } catch (error) {
